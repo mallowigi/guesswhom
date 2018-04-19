aa46536f0e1793c72df5f560da595e35ac982a41
mallowigi <heliosaian@gmail.com>
2018-04-19 23:16:27 +0300

    second commit

---
+++ src/components/Game.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Game</div>
+</template>
+
+<script>
+  export default {
+    name: 'Game',
+  };
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,18 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import App from '@/App';
+import Game from '@/components/Game';
+
 Vue.use(Router);
 
 export default new Router({
-  routes: [],
+  routes: [{
+    path: '/',
+    component: App,
+    children: [{
+      path: '',
+      component: Game,
+    }],
+  }],
 });
