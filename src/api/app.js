0d8354b6f759a3eeffb07b61b53f1b934b79e592
Elior Boukhobza <heliosaian@gmail.com>
2018-04-22 17:14:26 +0300

    Update app.js

--- src/api/app.js
+++ src/api/app.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-plusplus */
 import * as axios from 'axios';
 
-const SERVER_URL = process.env.SERVER_URL || 'http://localhost:5000';
+const SERVER_URL = 'https://guesswhomjs.herokuapp.com:5000';
 
 export default {
   getLogos() {
