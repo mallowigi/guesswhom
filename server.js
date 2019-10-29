a972eeda43c11f9a5d6a4c949958814493d8d206
Elior <eliorboukhobza@Eliors-MacBook-Pro.local>
2019-10-29 11:14:09 +0200

    netlfiuy test

--- server.js
+++ server.js
@@ -1,18 +1,4 @@
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const express = require('express');
-const serveStatic = require('serve-static');
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(serveStatic(__dirname + '/dist'));
-
-app.get('/logos', (req, res) => {
-  return res.json(require('./static/logos.json'));
-});
-
+const app = require('./src/server');
 const port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started ' + port);
+
+app.listen(port, () => console.log('server started ' + port));
